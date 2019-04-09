2e4346dec8073209e9d306bb691338550eedda36
Noura Ashraf Elsaadany <nouraelsaadany@hotmail.com>
2019-04-09 13:59:21 +0200

    es6

--- maines6.js
+++ maines6.js
@@ -9,60 +9,63 @@ const deleteButton = document.createElement('button');
 const input = document.getElementById('item');
 const search = document.getElementById('search');
 
+// let itemsArray = []
+// console.log(itemsArray)
 
-let itemsArray = []
-console.log(itemsArray)
-
-// creating lists 
-function liMaker(text){
-   const rowDiv = document.createElement('div');
-   rowDiv.className= "row ";
-   rowDiv.style.display="inline-block";
-
-   
-   const leftColumnDiv = document.createElement('div');
-   leftColumnDiv.className= "col-sm-4";
-   const li = document.createElement('li');
-   li.className="list-group-item list-group-item-info"
-   //li.id= Date.now();
-   li.textContent = text;
-   li.style.display="block";
-   leftColumnDiv.appendChild(li);
-   
-    const rightColumnDiv = document.createElement('div');
-    rightColumnDiv.className= "col-sm-4";
-    const button= document.createElement('button');
-    button.className="btn btn-outline-success my-2 my-sm-0";
-    button.textContent= "DELETE";
-    addBListen(button, rowDiv, itemsArray.length)
-    rightColumnDiv.append(button)
-
-    rowDiv.append(leftColumnDiv);
-    rowDiv.append(rightColumnDiv);
-    
-    ul.appendChild(rowDiv);
-
+class Task{
+constructor(itemsArray){
+this.itemsArray=[];
 }
 
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
+listMaker= (text)=>{
+const rowDiv = document.createElement('div');
+rowDiv.className= "row ";
+rowDiv.style.display="inline-block";
+
+
+const leftColumnDiv = document.createElement('div');
+leftColumnDiv.className= "col-sm-4";
+const li = document.createElement('li');
+li.className="list-group-item list-group-item-info"
+//li.id= Date.now();
+li.textContent = text;
+li.style.display="block";
+leftColumnDiv.appendChild(li);
+
+ const rightColumnDiv = document.createElement('div');
+ rightColumnDiv.className= "col-sm-4";
+ const button= document.createElement('button');
+ button.className="btn btn-outline-success my-2 my-sm-0";
+ button.textContent= "DELETE";
+ addBListen(button, rowDiv, itemsArray.length)
+ rightColumnDiv.append(button)
+
+ rowDiv.append(leftColumnDiv);
+ rowDiv.append(rightColumnDiv);
+ 
+ ul.appendChild(rowDiv);
+
+}
+
+//adding items 
+addingToList=()=>{
     if(input.value!==''){
         itemsArray.push(input.value);
         liMaker(input.value);
         input.value = "";
         localStorage.setItem('items', JSON.stringify(itemsArray));
     }
-});
+}
 
-// Filter 
 
-formSearch.addEventListener('keyup', function (e) {
-    e.preventDefault();
+//filer
+
+filterList=(search)=>{
     if(search.value!==''){
         let result = itemsArray.filter(isInList);
         const text = e.target.value;
-        for(let i = 0; i < ul.childNodes.length; i++){
+        ul.childNodes.forEach(search => {
             const rowText = ul.childNodes[i].childNodes[0].childNodes[0].childNodes[0].data.toLowerCase();
             console.log(rowText)
             if(rowText.indexOf(text) != -1){
@@ -72,23 +75,20 @@ formSearch.addEventListener('keyup', function (e) {
                 ul.childNodes[i].style.display = "none"
               //  console.log("sss", ul.childNodes[i]);
             }
-        }
+            
+        });
         
         localStorage.setItem('items', JSON.stringify(itemsArray));
     }
-});
+}
 
-function isInList(value) {
-    
+isInlist=(value)=>{
     if(value.includes(search.value)){
         return value.toLowerCase();
     }
-    
-  }
+}
 
-
-
-const addBListen = (button, child, size) =>
+addBListen = (button, child, size) =>
     button.addEventListener('click', function () {
         let clicked=confirm("Are You Sure You want to delete this task ?");
         alert(clicked);
@@ -98,28 +98,30 @@ if(clicked==true){
     localStorage.setItem('items', JSON.stringify(itemsArray));
     console.log(itemsArray)
 }
-           
- 
-      
     });
 
-const loadFromLocalStorage = ()=>{
+
+loadFromLocalStorage=()=>{
     const temp = JSON.parse(localStorage.getItem('items'));
     console.log("temp "+ temp)
     itemsArray = temp !== null? temp:[];
     itemsArray.forEach(element => {
         liMaker(element)
     });
-};
+
+}
+
 
 loadFromLocalStorage();
+itemsX=  localStorage.getItem('items', JSON.stringify(itemsArray));
+result = itemsArray.indexOf(search.value);
+}
 
-    var itemsX=  localStorage.getItem('items', JSON.stringify(itemsArray));
-    console.log(itemsArray);
-
-
-console.log(search.value)
-var result = itemsArray.indexOf(search.value);
-
-
-   console.log(result)
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+addingToList();
+});
+formSearch.addEventListener('keyup',function(e){
+    e.preventDefault();
+    filterList(e);
+})
